a49178b4cb5fd6142f2cee46e7a8b822d7954136
Aleksandr Tolkanev <allcars61@gmail.com>
2023-04-24 18:30:45 +0300

    Update vk.py

--- KURS_VK/vk.py
+++ KURS_VK/vk.py
@@ -1,38 +1,67 @@
-def main():
-    vk_user_id = input("Введите id пользователя вконтакте: ")
-    yandex_disk_token = input("Введите токен Яндекс.Диска: ")
+import sys
+import vk_api
+import requests
+import json
+from datetime import datetime
+from yadisk import YaDisk
 
-    vk_session = vk_api.VkApi(token='YOUR_TOKEN') # 'YOUR_TOKEN' заменить на токен ВК
+
+def get_photos_info(vk_user_id):
+    vk_session = vk_api.VkApi(token='YOUR_TOKEN')  # 'YOUR_TOKEN' заменить на токен ВК
     vk = vk_session.get_api()
 
-    photos = vk.photos.get(owner_id=vk_user_id, album_id='profile', photo_sizes=1)['items']
+    try:
+        user_info = vk.users.get(user_ids=vk_user_id)[0]
+    except:
+        print("Ошибка! Пользователь не найден.")
+        sys.exit()
 
-    count = min(len(photos), 5)
+    user_name = user_info['first_name'] + '_' + user_info['last_name']
+    photos = vk.photos.get(owner_id=vk_user_id, album_id='profile', photo_sizes=1, extended=1)['items']
+    photos_info = {}
 
-    disk = YandexDisk(token=yandex_disk_token)
-
-    folder_name = 'photos_from_vk'
-    disk.mkdir(folder_name)
-
-    for i in range(count):
-        photo = sorted(photos[i]['sizes'], key=lambda x: x['width'], reverse=True)[0]
+    for i in range(len(photos)):
+        photo = sorted(photos[i]['sizes'], key=lambda x: (x['width'], x['height']), reverse=True)[0]
         url = photo['url']
-
-        name = str(photos[i]['likes']['count']) + '.jpg'
+        likes = photos[i]['likes']['count']
+        date = datetime.utcfromtimestamp(photos[i]['date']).strftime('%Y-%m-%d_%H-%M-%S')
+        file_name = str(likes) + '_' + date + '.jpg'
         size = photo['type']
         width = photo['width']
         height = photo['height']
 
-        r = requests.get(url)
-        if r.status_code == 200:
-            disk.upload(r.content, folder_name + '/' + name)
+        photos_info[likes] = [url, file_name, size, width, height]
 
-        photo_info = {'file_name': name, 'size': size, 'width': width, 'height': height}
+    return photos_info, user_name
+
+
+def save_photos_to_disk(photos_info, user_name, yandex_disk_token):
+    disk = YaDisk(token=yandex_disk_token)
+    folder_name = user_name + '_photos_from_vk'
+    disk.mkdir(folder_name)
+
+    for likes in sorted(photos_info.keys(), reverse=True):
+        url = photos_info[likes][0]
+        file_name = photos_info[likes][1]
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            disk.upload(r.content, folder_name + '/' + file_name)
+
+        photo_info = {'file_name': file_name, 'likes': likes}
         with open('photos_info.json', 'a') as file:
             file.write(json.dumps(photo_info) + '\n')
 
     print('Готово!')
 
+
+def main():
+    vk_user_id = input("Введите id пользователя вконтакте: ")
+    yandex_disk_token = input("Введите токен Яндекс.Диска: ")
+
+    photos_info, user_name = get_photos_info(vk_user_id)
+    save_photos_to_disk(photos_info, user_name, yandex_disk_token)
+
+
 if __name__ == '__main__':
     main()
-
